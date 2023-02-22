33df8a297873c97b4e9b1e3883433fda85dfc725
Michal Baran <michal.baran@join.com>
2023-02-22 10:27:52 +0100

    joinTheme update

--- apps/docs/joinTheme.config.js
+++ apps/docs/joinTheme.config.js
@@ -1,8 +1,6 @@
-import { SEO_IMG_DEFAULT, DOCS_URL } from "@calcom/lib/constants";
+import { SEO_IMG_DEFAULT } from "@calcom/lib/constants";
 
 const themeConfig = {
-  github: "https://github.com/calcom/cal.com",
-  docsRepositoryBase: "https://github.com/calcom/cal.com/blob/main/apps/docs/pages",
   titleSuffix: " | Join.com",
   logo: <h4 className="m-0">Join.com</h4>,
   head: (
@@ -19,13 +17,9 @@ const themeConfig = {
         name="og:description"
         content="Explore advice and explanations for all of our features, and discover new tips and tricks to get the most out of your subscription."
       />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content={SEO_IMG_DEFAULT} />
-      <meta name="twitter:site:domain" content="docs.cal.com" />
-      <meta name="twitter:url" content={DOCS_URL} />
-      <meta name="og:title" content="Cal.com Documentation" />
+      <meta name="og:title" content="Join.com Documentation" />
       <meta name="og:image" content={SEO_IMG_DEFAULT} />
-      <meta name="apple-mobile-web-app-title" content="Cal.com Docs" />
+      <meta name="apple-mobile-web-app-title" content="Join.com Docs" />
 
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/join-favicon-32x32.png" />
@@ -41,7 +35,7 @@ const themeConfig = {
   nextLinks: true,
   footer: true,
   footerEditLink: "Edit this page on GitHub",
-  footerText: <>© {new Date().getFullYear()} Cal.com, Inc. All rights reserved.</>,
+  footerText: <>© {new Date().getFullYear()} JOIN Solutions AG. All rights reserved.</>,
 };
 
 export default themeConfig;
