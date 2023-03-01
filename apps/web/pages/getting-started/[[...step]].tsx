10354e30213123573926f1c892055f44ee134723
Michal Baran <michal.baran@join.com>
2023-03-01 14:47:09 +0100

    feat: JOIN-27734 update registration flow and integration

--- apps/web/pages/getting-started/[[...step]].tsx
+++ apps/web/pages/getting-started/[[...step]].tsx
@@ -14,13 +14,11 @@ import type { inferSSRProps } from "@lib/types/inferSSRProps";
 
 import { ConnectedCalendars } from "@components/getting-started/steps-views/ConnectCalendars";
 import { SetupAvailability } from "@components/getting-started/steps-views/SetupAvailability";
-import UserProfile from "@components/getting-started/steps-views/UserProfile";
-import { UserSettings } from "@components/getting-started/steps-views/UserSettings";
 
 export type IOnboardingPageProps = inferSSRProps<typeof getServerSideProps>;
 
-const INITIAL_STEP = "user-settings";
-const steps = ["user-settings", "connected-calendar", "setup-availability", "user-profile"] as const;
+const INITIAL_STEP = "connected-calendar";
+const steps = ["connected-calendar", "setup-availability"] as const;
 
 const stepTransform = (step: (typeof steps)[number]) => {
   const stepIndex = steps.indexOf(step);
@@ -45,10 +43,6 @@ const OnboardingPage = (props: IOnboardingPageProps) => {
 
   const headers = [
     {
-      title: `${t("welcome_to_cal_header", { appName: APP_NAME })}`,
-      subtitle: [`${t("we_just_need_basic_info")}`, `${t("edit_form_later_subtitle")}`],
-    },
-    {
       title: `${t("connect_your_calendar")}`,
       subtitle: [`${t("connect_your_calendar_instructions")}`],
       skipText: `${t("connect_calendar_later")}`,
@@ -59,10 +53,6 @@ const OnboardingPage = (props: IOnboardingPageProps) => {
         `${t("set_availability_getting_started_subtitle_1")}`,
         `${t("set_availability_getting_started_subtitle_2")}`,
       ],
-    },
-    {
-      title: `${t("nearly_there")}`,
-      subtitle: [`${t("nearly_there_instructions")}`],
     },
   ];
 
@@ -108,15 +98,11 @@ const OnboardingPage = (props: IOnboardingPageProps) => {
               <Steps maxSteps={steps.length} currentStep={currentStepIndex + 1} navigateToStep={goToIndex} />
             </div>
             <StepCard>
-              {currentStep === "user-settings" && <UserSettings user={user} nextStep={() => goToIndex(1)} />}
-
-              {currentStep === "connected-calendar" && <ConnectedCalendars nextStep={() => goToIndex(2)} />}
+              {currentStep === "connected-calendar" && <ConnectedCalendars nextStep={() => goToIndex(1)} />}
 
               {currentStep === "setup-availability" && (
-                <SetupAvailability nextStep={() => goToIndex(3)} defaultScheduleId={user.defaultScheduleId} />
+                <SetupAvailability defaultScheduleId={user.defaultScheduleId} />
               )}
-
-              {currentStep === "user-profile" && <UserProfile user={user} />}
             </StepCard>
             {headers[currentStepIndex]?.skipText && (
               <div className="flex w-full flex-row justify-center">
